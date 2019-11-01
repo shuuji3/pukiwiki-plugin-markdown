65f0ad26984dd954a2659039dc02f5716a74876a
TAKAHASHI Shuuji <shuuji3@gmail.com>
2019-11-02 02:02:38 +0900

    ✨ Add the syntax highlight.

--- markdown.inc.php
+++ markdown.inc.php
@@ -1,22 +1,41 @@
 <?php
 
-function plugin_markdown_header()
-{
+function plugin_markdown_header() {
     return <<<'EOS'
 <script src="https://unpkg.com/markdown-it/dist/markdown-it.min.js"></script>
+<script src="https://unpkg.com/highlightjs"></script>
 <script>
   document.addEventListener("DOMContentLoaded", () => {
+    // Load GitHub style for <code> tag
+    const style = document.createElement('link');
+    style.href = 'https://unpkg.com/highlightjs/styles/github-gist.css';
+    style.type = 'text/css';
+    style.rel = 'stylesheet';
+    document.head.appendChild(style);
+
     const mdBodies = document.querySelectorAll('.md-body');
     mdBodies.forEach(mdBody => {
-      mdBody.innerHTML = window.markdownit().render(mdBody.textContent);
+      mdBody.innerHTML = window.markdownit({
+        linkify: true,
+        langPrefix:   'language-',
+        highlight: function (str, lang) {
+          if (lang && hljs.getLanguage(lang)) {
+            try {
+              return '<pre class="hljs"><code>' +
+                hljs.highlight(lang, str, true).value +
+               '</code></pre>';
+            } catch (__) {}
+          }
+          return '<pre class="hljs"><code>' + window.markdownit().utils.escapeHtml(str) + '</code></pre>';
+        },
+      }).render(mdBody.textContent);
     });
   });
 </script>
 EOS;
 }
 
-function plugin_markdown_convert()
-{
+function plugin_markdown_convert() {
     $header = "";
     if (!defined("PLUGIN_MARKDOWN_LOADED")) {
         define("PLUGIN_MARKDOWN_LOADED", "LOADED");
